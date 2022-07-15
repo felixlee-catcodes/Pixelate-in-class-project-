b69d911a4788f036536bcd1d4a84afdbbfcd98ad
xyshtd <mhu225@gmail.com>
2022-07-14 22:38:31 -0400

    added change color by select dropdown

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 // Your code here
 const table = document.querySelector('table');
-
+const select = document.querySelector('select')
 const addRow = document.getElementById('add-row');
+let pickedColor = 'red';
+let num = 5
 
-const pickedColor = 'red';
 
+//add a row; column num pre-defined for now
+addRow.addEventListener('click', () => {
+    const tr = document.createElement('tr');
+    for (let i = 0; i < num; i++){
+        const td = document.createElement('td');
+        tr.appendChild(td);
+    }
+    table.appendChild(tr);
+});
+
+//change color by select dropdown
+select.addEventListener('change', (ev) => {
+    pickedColor=ev.target.value
+})
+
+
+//toggle on and off color
 table.addEventListener('click', (ev)=>{
-    
     if(ev.target.tagName === 'TD'){
         if(ev.target.className === pickedColor){
             ev.target.className = ''
@@ -15,19 +32,5 @@ table.addEventListener('click', (ev)=>{
         }
     }
 })
-
-addRow.addEventListener('click', () => {
-    const tr = document.createElement('tr');
-
-    for (let i = 0; i < 7; i++){
-        const td = document.createElement('td');
-        tr.appendChild(td);
-    }
-    table.appendChild(tr);
-    const td = document.querySelectorAll('td');
-    console.log(tr)
-});
-
-
 
 
